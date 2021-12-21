492516f52b3a8392b993ac51ae3760c0afdbb425
zhmeishi <zhmeishi@gmail.com>
2021-12-20 20:40:43 -0600

    script

--- script.sh
+++ script.sh
@@ -1,19 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 #
-#SBATCH --output=./log/l_%j.log
-#SBATCH --error=./log/e_%j.log
+#SBATCH --output=./log/l_device_%j.log
+#SBATCH --error=./log/e_device_%j.log
 #***SBATCH --partition=batch_default ***
 # 
 # 1 node, 1 CPU per node (total 1 CPU), wall clock time of hours
 #
 #SBATCH -N 1                  ## Node count
-#SBATCH --ntasks-per-node=3   ## Processors per node
-#SBATCH --ntasks=1             ## Tasks
+#SBATCH --ntasks-per-node=4   ## Processors per node
+#SBATCH --ntasks=1            ## Tasks
 #SBATCH --gres=gpu:1          ## GPUs
-#SBATCH --cpus-per-task=3     ## CPUs per task; number of threads of each task
+#SBATCH --cpus-per-task=4     ## CPUs per task; number of threads of each task
 #SBATCH -t 256:00:00          ## Walltime
 #SBATCH --mem=20GB
-#SBATCH -p ppc
-cd /srv/home/zhmeishi/robust/euler
-source ~/.bashrc_wml
-python run_file --conf ./conf/yamlname.yaml
+#SBATCH -p lianglab
+cd ~/Euler_Liang_tutorial
+source ~/.zshrc
+python train.py
--- submit.py
+++ submit.py
@@ -1,52 +1,43 @@
 import os
 import argparse
-import json
 
 parser = argparse.ArgumentParser("Training model")
 
-parser.add_argument('--ppc', action='store_true')
-parser.add_argument('--liang', action='store_true')
 parser.add_argument('--cpu', action='store_true')
+parser.add_argument('--lianglab', action='store_true')
+parser.add_argument('--wacc', action='store_true')
+parser.add_argument('--reseach', action='store_true')
 
 args = parser.parse_args()
 config_file_path = args.config
-run_file = args.run
 cpu = args.cpu
 
-run_file = os.path.basename(run_file)
-
-config_file = os.path.basename(config_file_path)[:-5]
-conf_type = os.path.basename(config_file_path)[-4:]
-
-assert("raw" not in config_file)
-if run_file == "main.py":
-    read_file = "./script/raw.sh"
-else:
-    raise("File is not defined")
+read_file = "./script.sh"
 
 f = open(read_file)
 text = f.read()
 f.close()
-if args.liang:
-    text = text.replace(
-        "#SBATCH -p ppc", "#SBATCH -p lianglab \n#SBATCH --qos=lianglab_owner")
-elif not args.ppc:
-    text = text.replace("#SBATCH -p ppc", "")
-if cpu:
+if args.wacc:
+    text = text.replace("#SBATCH -p lianglab", "#SBATCH -p wacc")
+    text = text.replace("device", "wacc")
+    text = text.replace("#SBATCH -t 256:00:00          ## Walltime", "#SBATCH -t 1:00:00          ## Walltime")
+elif args.reseach:
+    text = text.replace("#SBATCH -p lianglab", "#SBATCH -p reseach")
+    text = text.replace("device", "research")
+elif cpu:
     text = text.replace("#SBATCH --gres=gpu:1          ## GPUs", "")
+    text = text.replace("device", "cpu")
+elif not args.lianglab:
+    text = text.replace("#SBATCH -p lianglab", "")
+    text = text.replace("device", "lianglab")
+else:
+    text = text.replace("device", "none")
 
-path = "./script/"+config_file+".sh"
-
-text = text.replace("run_file", run_file)
-text = text.replace("yamlname", config_file)
+path = "./tmp.sh"
 
 f = open(path, "w")
 f.write(text)
 f.close()
 
-if not os.path.exists("exp"):
-    os.makedirs("exp")
-exp_path = os.path.join("exp", config_file)
-if not os.path.exists(exp_path):
-    os.makedirs(exp_path)
 os.system("sbatch " + path)
+os.remove(path)
