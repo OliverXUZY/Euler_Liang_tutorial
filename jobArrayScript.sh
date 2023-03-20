9cfb38bc6c88d808222d834f01491c11bc309a42
OliverXUZY <zxu68341@icloud.com>
2023-03-20 12:34:51 -0500

    job array

---
+++ jobArrayScript.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+#SBATCH --mail-user=zxu444@wisc.edu
+#SBATCH --mail-type=ALL
+#SBATCH -J Mm  # give the job a name   
+#***SBATCH --partition=batch_default ***
+# 
+# 1 node, 1 CPU per node (total 1 CPU), wall clock time of hours
+#
+#SBATCH -N 1                  ## Node count
+#SBATCH --ntasks-per-node=4   ## Processors per node
+#SBATCH --ntasks=1            ## Tasks
+#SBATCH --gres=gpu:1          ## GPUs
+#SBATCH --cpus-per-task=4     ## CPUs per task; number of threads of each task
+#SBATCH -t 256:00:00          ## Walltime
+#SBATCH --mem=40GB
+#SBATCH -p research
+#SBATCH --error=./log/array/python_array_job_slurm_%A_%a.err
+#SBATCH --output=./log/array/python_array_job_slurm_%A_%a.out
+source ~/.bashrc
+
+sleep 5
+echo 5
+echo "SLURM_JOBID: " $SLURM_JOBID
+echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
+
